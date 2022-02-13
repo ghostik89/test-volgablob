59c3f5b2c77bdfe27cf907442b484b3295514c08
Ekaterina Tarapatina <facktartar@gmail.com>
2022-02-13 18:35:21 +0300

    [feat] add routes for app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import {MainPage} from "./pages/MainPage";
+import {CommentPage} from "./pages/CommentPage";
 
 export const App = () => {
   return (
-    <div>
-      react
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route path={"/"} element={<MainPage/>}/>
+              <Route path={"/:id"} element={<CommentPage/>}/>
+          </Routes>
+      </BrowserRouter>
   );
 }
